36ee79bcaea4334d5c28a389664a4d3780d4ba8a
buce <378741605@qq.com>
2016-01-22 11:22:12 +0800

    更新

--- src/me/websocketchat/Test.java
+++ src/me/websocketchat/Test.java
@@ -8,25 +8,5 @@ import javax.websocket.server.ServerEndpoint;
  */
 @ServerEndpoint(value = "/test")
 public class Test {
-    @OnOpen
-    public void start(Session session){
-        System.out.println("test-session "+session.getId()+" open.");
-    }
-
-    @OnMessage
-    public void process(Session session, String message){
-        System.out.println("test-session " + session.getId() + " msg.");
-    }
-
-    @OnClose
-    public void end(Session session){
-        System.out.println("test-session " + session.getId() + " close.");
-    }
-
-    @OnError
-    public void error(Session session, java.lang.Throwable throwable){
-        System.err.println("test-session " + session.getId() + " error:" + throwable);
-    }
-
 
 }
